1376d656fbaf0bb42eda9840853209aed8136ff0
Eric Mc Sween <eric.mcsween@auf.org>
2012-02-03 15:10:39 -0500

    [#2534] Version 0.6: corrigé des problèmes de packaging

--- setup.py
+++ setup.py
@@ -3,15 +3,17 @@
 from setuptools import setup, find_packages
 
 name = 'auf.django.references'
-version = '0.5'
+version = '0.6'
 
-setup(name=name,
-      version=version,
-      description="Modèles Django pour les données de référence de l'AUF",
-      author='Éric Mc Sween',
-      author_email='eric.mcsween@auf.org',
-      url='http://pypi.auf.org/%s' % name,
-      license='GPL',
-      packages=find_packages(),
-      zip_safe=False,
-     )
+setup(
+    name=name,
+    version=version,
+    description="Modèles Django pour les données de référence de l'AUF",
+    author='Éric Mc Sween',
+    author_email='eric.mcsween@auf.org',
+    url='http://pypi.auf.org/%s' % name,
+    license='GPL',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    zip_safe=False,
+)
