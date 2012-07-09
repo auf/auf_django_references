d64e45676794589ca6df33452b2d814ba654e07c
Eric Mc Sween <eric.mcsween@auf.org>
2012-07-09 13:17:10 -0400

    Version 0.20: Retiré les interfaces d'admin

--- auf/django/references/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from auf.django.references import models as ref
-
-admin.site.register(ref.Region)
-admin.site.register(ref.Bureau)
-admin.site.register(ref.Implantation)
-admin.site.register(ref.Pays)
-admin.site.register(ref.Etablissement)
